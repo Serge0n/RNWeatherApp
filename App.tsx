286680289af00d2ee34337e6d8771f7b7ec6d905
Serge0n <serge0nnnnn@gmail.com>
2023-07-10 19:47:04 +0200

    Theme init

--- App.tsx
+++ App.tsx
@@ -1,25 +1,22 @@
-import { NavigationContainer } from '@react-navigation/native'
+import {
+  DarkTheme,
+  DefaultTheme,
+  NavigationContainer,
+} from '@react-navigation/native'
 import { NavStack } from 'navigation'
 import { StatusBar, useColorScheme } from 'react-native'
 import RNBootSplash from 'react-native-bootsplash'
-import { Colors } from 'react-native/Libraries/NewAppScreen'
 
 export function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark'
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  }
-
   const hideSplash = () => RNBootSplash.hide({ fade: true })
 
   return (
-    <NavigationContainer onReady={hideSplash}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-
+    <NavigationContainer
+      onReady={hideSplash}
+      theme={isDarkMode ? DarkTheme : DefaultTheme}>
+      <StatusBar />
       <NavStack />
     </NavigationContainer>
   )
---
+++ src/components/Text.tsx
@@ -0,0 +1,8 @@
+import { useTheme } from '@react-navigation/native'
+import { Text as RNText, TextProps } from 'react-native'
+
+export function Text({ style, ...restProps }: TextProps) {
+  const { colors } = useTheme()
+
+  return <RNText style={[{ color: colors.text }, style]} {...restProps} />
+}
