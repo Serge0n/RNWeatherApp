873324e933255f6b046dee4aee7b02dc29d5a610
Serge0n <serge0nnnnn@gmail.com>
2023-07-12 16:06:43 +0200

    Icon, AsyncStoradge + logic init

--- src/components/CityItem.tsx
+++ src/components/CityItem.tsx
@@ -1,13 +1,23 @@
 import { useTheme } from '@react-navigation/native'
 import { Text } from 'components'
-import { Pressable, PressableProps } from 'react-native'
+import { Pressable, PressableProps, StyleProp, ViewStyle } from 'react-native'
+import AntDesignIcon from 'react-native-vector-icons/AntDesign'
 
-interface CityItemProps {
+type CityItemProps = {
   city: any
-  onPress: PressableProps['onPress']
-}
+  isFavorite?: boolean
+  onStarPress: () => void
+  style?: StyleProp<ViewStyle>
+} & PressableProps
 
-export function CityItem({ city, onPress }: CityItemProps) {
+export function CityItem({
+  isFavorite = false,
+  city,
+  onPress,
+  onStarPress,
+  style,
+  ...restProps
+}: CityItemProps) {
   const { colors } = useTheme()
 
   return (
@@ -18,11 +28,23 @@ export function CityItem({ city, onPress }: CityItemProps) {
           borderWidth: 2,
           borderColor: colors.border,
           opacity: pressed ? 0.5 : 1.0,
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
         },
+        style,
       ]}
       onPress={onPress}
+      {...restProps}
     >
       <Text>{`${city.name}, ${city.sys.country}, ${city.main.temp} °C, ${city.weather[0].main}`}</Text>
+      <Pressable onPress={onStarPress}>
+        <AntDesignIcon
+          name='star'
+          size={30}
+          color={isFavorite ? colors.text : colors.border}
+        />
+      </Pressable>
     </Pressable>
   )
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,51 +1,99 @@
+import { useAsyncStorage } from '@react-native-async-storage/async-storage'
 import { useNavigation } from '@react-navigation/native'
 import { CityItem, TextInput } from 'components'
 import { NavStackProps } from 'navigation'
-import { useState } from 'react'
-import { FlatList, View } from 'react-native'
+import { useEffect, useState } from 'react'
+import { ActivityIndicator, FlatList, View } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 const API_WEATHER_KEY = '889f0cd84330b182bf974584d4b2966d'
 
 export function Home() {
   const { navigate } = useNavigation<NavStackProps<'Home'>['navigation']>()
+  const [isLoading, setIsLoading] = useState(false)
   const { top, bottom } = useSafeAreaInsets()
   const [cities, setCities] = useState<Array<any>>([])
+  const [favorites, setFavorites] = useState<Array<any>>([])
+  const { getItem, setItem } = useAsyncStorage('favorites')
 
   const getCities = async (city: string) => {
     try {
+      setIsLoading(true)
       const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_WEATHER_KEY}`,
       )
-      const cityWeather = await response.json()
-      setCities([cityWeather])
-      console.log(cityWeather)
+      const cityData = await response.json()
+      const isDuplicate = favorites.some((item) => item.id === cityData.id)
+
+      !isDuplicate && setCities([cityData])
     } catch (error) {
       console.log(error)
+    } finally {
+      setIsLoading(false)
     }
+  }
+
+  const readItemsFromFavorites = async () => {
+    const item = await getItem()
+    item && setFavorites(JSON.parse(item))
+  }
+
+  const writeItemsToFavorites = async (newValue: any) => {
+    await setItem(JSON.stringify([...favorites, newValue]))
+    setFavorites([...favorites, newValue])
+    setCities([])
+  }
+
+  const removeFavorite = async (newValue: any) => {
+    const filteredFavorites = favorites.filter(
+      (item) => newValue.id !== item.id,
+    )
+    await setItem(JSON.stringify(filteredFavorites))
+    setFavorites(filteredFavorites)
   }
 
   const onCityPress = () => navigate('CityWeather')
 
+  useEffect(() => {
+    readItemsFromFavorites()
+  }, [])
+
   return (
-    <View
-      style={{
-        flex: 1,
-        paddingTop: top,
-        paddingBottom: bottom,
-        paddingHorizontal: 16,
-      }}
-    >
+    <View style={{ flex: 1, paddingTop: top, paddingBottom: bottom }}>
       <TextInput
         placeholder='Enter the city'
+        onChangeText={() => setCities([])}
         onSubmitEditing={({ nativeEvent }) => getCities(nativeEvent.text)}
+        style={{ marginHorizontal: 16 }}
       />
 
       <FlatList
-        style={{ marginTop: 16 }}
+        style={{ marginTop: 16, paddingHorizontal: 16 }}
         data={cities}
+        ListHeaderComponent={
+          <View>
+            {favorites.map((item) => {
+              return (
+                <CityItem
+                  key={item.id}
+                  isFavorite
+                  city={item}
+                  onStarPress={() => removeFavorite(item)}
+                  onPress={onCityPress}
+                  style={{ marginBottom: 16 }}
+                />
+              )
+            })}
+
+            {isLoading && <ActivityIndicator />}
+          </View>
+        }
         renderItem={({ item }) => (
-          <CityItem city={item} onPress={onCityPress} />
+          <CityItem
+            city={item}
+            onPress={onCityPress}
+            onStarPress={() => writeItemsToFavorites(item)}
+          />
         )}
       />
     </View>
